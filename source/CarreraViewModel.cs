1d8547013d872f2fa2e9810460c6871aba42de8b
medehop <m.linkenheil@online.de>
2017-01-05 15:37:49 +0100

    changed project structure

--- source/CarreraViewModel.cs
+++ source/CarreraViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using Caliburn.Micro;
 using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Carrera
 {
